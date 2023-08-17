22c48bb2d6aa349cadac667bd4762c7770d5e717
Sky <ljl83405364@163.com>
2023-08-17 18:10:58 +0800

    1

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8081;
 // 创建express 实例
 const app = express();
 // 当路由url匹配为'/'时，执行function，返回Hello World
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+//app.get('/', function (req, res) {
+//  res.send('Hello World');
+//});
 // 配置 静态资源文件 
-app.use(express.static(__dirname + '/apps'))
+app.use(express.static('apps'))
 // 监听端口号
 app.listen(PORT,()=>{
     console.log('服务已启动')
