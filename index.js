1196db77eda097e3b3a3ec691005dc5742329411
Sky <ljl83405364@163.com>
2023-08-17 17:34:27 +0800

    8

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const express = require('express');
-const axios = require('axios');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+    fs.readFile("apps/index.html",(err,data)=>{
+      res.send(data);
 
-    res.send(repos);
+    })
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
