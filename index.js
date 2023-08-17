6b6b5b05ff0de224f6665a5184f468504e757da1
Sky <ljl83405364@163.com>
2023-08-17 17:55:30 +0800

    6

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const server = express();
 
 const PORT = process.env.PORT || 8081;
-server.listen(PORT, function () { //在3001端口启动
-    console.log('Example app listening on port 3001');
+// 创建express 实例
+const app = express();
+// 配置 静态资源文件 
+app.use(express.static(__dirname + '/apps'))
+// 监听端口号
+app.listen(PORT,()=>{
+    console.log('服务已启动')
 })
-
-server.use('', express.static('./apps/'))
